6273d43f65279d800a37a5dd9b34488d2cea54a1
kimihito <tatsurotamashiro@gmail.com>
2012-06-11 22:57:23 +0900

    chapter13

---
+++ cgi-bin/countertest.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+from simpleappserver import expose, test
+from httphandler import Response
+from simpletemplate import SimpleTemplate
+
+@expose
+def index(_request, d ={'counter':0}):
+  body = """<html><body><p>${counter}</p></body></html>"""
+  res = Response()
+  t = SImpleTemplate(body)
+  body = t.render(d)
+  d['counter'] += 1
+  res.set_body(body)
+  return res
+
+if __name__ == '__main__':
+  test()
